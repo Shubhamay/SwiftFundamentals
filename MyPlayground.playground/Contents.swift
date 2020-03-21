48c647a7be3638fff9d391faea7ae6712e2be89f
shubhamd <shubhamd@in-shubhamd.local>
2020-03-21 17:07:44 +0530

    added optional

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,3 +1,16 @@
 let a = 4.0
 print(type(of: a))
 let b = 1 + 1;
+
+let inta = Double(5 / 2)
+print(inta)
+
+//Using Optional
+var optionalA : Int?
+
+if optionalA != nil {
+    print(optionalA!+5)
+} else {
+        optionalA = 1
+        print(optionalA!+5)
+}
